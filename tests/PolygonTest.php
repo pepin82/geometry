1d332e44e1b7b961c01c43575a3a9e1e553bb1e5
pepin82 <pepin.1982*>
2015-02-04 15:58:39 -0300

    fix test case before pull request

--- tests/PolygonTest.php
+++ tests/PolygonTest.php
@@ -5,21 +5,6 @@ use Geometry\Polygon;
 class PolygonTest extends \PHPUnit_Framework_TestCase {
 
     /**
-     * @var \Geometry\Polygon
-     */
-    private $polygon;
-
-    public function setUp() {
-        $this->polygon = new Polygon(new Points(array(
-            new Point(-1, -1),
-            new Point(-1, 1),
-            new Point(1, 1),
-            new Point(1, -1),
-            new Point(-1, -1)
-        )));
-    }
-
-    /**
      * @expectedException \InvalidArgumentException
      */
     public function test_ConstructorWithOpenPoints_MustThrowException() {
@@ -30,10 +15,30 @@ class PolygonTest extends \PHPUnit_Framework_TestCase {
         )));
     }
 
-    public function test_Contains() {
-        $this->assertFalse($this->polygon->contains(new Point(-2, -2)));
-        $this->assertTrue($this->polygon->contains(new Point(0, 0)));
-        $this->assertTrue($this->polygon->contains(new Point(1, 0)));
-        $this->assertTrue($this->polygon->contains(new Point(1, 1)));
+    public function test_Contains_Case1() {
+        $polygon = new Polygon(new Points(array(
+            new Point(-1, -1),
+            new Point(-1, 1),
+            new Point(1, 1),
+            new Point(1, -1),
+            new Point(-1, -1)
+        )));
+
+        $this->assertFalse($polygon->contains(new Point(-2, -2)));
+        $this->assertTrue($polygon->contains(new Point(0, 0)));
+        $this->assertTrue($polygon->contains(new Point(1, 0)));
+        $this->assertTrue($polygon->contains(new Point(1, 1)));
+    }
+
+    public function test_Contains_Case2() {
+        $polygon = new Polygon(new Points(array(
+            new Point(30.517455, 50.439635),
+            new Point(30.519381, 50.438067),
+            new Point(30.521543, 50.439133),
+            new Point(30.518684, 50.440199),
+            new Point(30.517455, 50.439635)
+        )));
+
+        $this->assertTrue($polygon->contains(new Point(30.51954, 50.43899)));
     }
 }
